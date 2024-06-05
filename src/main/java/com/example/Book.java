8a1819c71c3d1f03d8090968b3568f98d03c16a6
vibhas123 <vibhasbachu01@gmail.com>
2024-06-05 14:24:04 +0530

    vibhas java2 assignment

--- src/main/java/com/example/Book.java
+++ src/main/java/com/example/Book.java
@@ -1,3 +1,60 @@
+package com.example;
+
+
+
+/**
+ * Book
+ */
+public class Book {
+    private String title;
+    private String author;
+    private int  copies;
+    static int count;
+    public Book(String title, String author, int copies) {
+        this.title = title;
+        this.author = author;
+        this.copies = copies;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getAuthor() {
+        return author;
+    }
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+    public int getCopies() {
+        return copies;
+    }
+    public void setCopies(int copies) {
+        if(copies>0){
+
+        
+        this.copies = copies;
+        }
+    }
+    public static int getCount() {
+        return count;
+    }
+    
+    public void displayBookDetails(){
+        System.out.println("title:"+title);
+        System.out.println("author:"+author);
+        System.out.println("copies:"+copies);
+    }
+    public static int totalBooks(){
+        
+        return count;
+    }
+
+    
+
+    
+}
 /*
  * Requirements
 Class Definition:
